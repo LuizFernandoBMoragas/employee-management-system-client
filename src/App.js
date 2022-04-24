f4203f572364f24c997d2334cce0a4804d14c88e
Luiz Fernando Barros Moragas <luiz.moragas@hotmail.com>
2022-04-24 21:19:40 +0100

    first app style created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="information">
+        <h1>New Employee Data</h1>
+        <label>Name: </label>
+        <input type="text" />
+        <label>Age: </label>
+        <input type="number" />
+        <label>Country: </label>
+        <input type="text" />
+        <label>Position: </label>
+        <input type="text" />
+        <label>Wage (year): </label>
+        <input type="number" />
+        <button>Register Employee</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
